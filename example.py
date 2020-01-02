3785fbedc85d8748edf93768d4410ff0fef59a36
yogiz <ramadhanright@gmail.com>
2020-01-02 21:09:26 +0800

    add lq45 stock list for example

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 from ipotscrape import scrape_to_csv, scrape_to_db
 
 mode = 0 			# mode 0 save to db, mode 1 save to csv 
-symbols = ['bbca', 'ggrm', 'jast']
+symbols = ['adhi','adro','akra','antm','asii','asri','bbca','bbni','bbri','bbtn','bksl','bmri','bsde','cpin','elsa','excl','ggrm','hmsp','icbp','inco','indf','indy','inkp','intp','itmg','jsmr','klbf','lpkr','lppf','medc','mncn','pgas','ptba','ptpp','scma','smgr','sril','ssms','tlkm','tpia','untr','unvr','wika','wsbp','wskt','jast']
 
 for symbol in symbols :
 	if(mode == 0) :
